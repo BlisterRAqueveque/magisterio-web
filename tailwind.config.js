fe0f7094c7b9b0b68b4197265f326e980dd87193
BlisterRAqueveque <raqueveque@blister.com.ar>
2024-07-27 18:18:27 -0300

    Feat: change general bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mag: {
+          50: "#fffbea",
+          100: "#fff1c5",
+          200: "#ffe485",
+          300: "#ffcf46",
+          400: "#ffb91b",
+          500: "#fa9200",
+          600: "#e26e00",
+          700: "#bb4a02",
+          800: "#983908",
+          900: "#7c2f0b",
+          950: "#481600",
+        },
+      },
+    },
   },
   plugins: [],
 };
